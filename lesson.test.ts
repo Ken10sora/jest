771a3d5ac373c3b5dde0013a855c857694a34779
Ken10 <76900596+Ken10sora@users.noreply.github.com>
2024-01-21 20:34:48 +1100

    Add files via upload

---
+++ lesson.test.ts
@@ -0,0 +1,25 @@
+// ▪️復習や新しいテストコードを記述
+test('testを使用してJapanを試す', () => {
+  const result = 'Japan'
+  const expected = 'Japan'
+  expect(result).toBe(expected)
+})
+
+test('contains Hearthflame Mask in Ogerpon', () => {
+  expect(Ogerpon).toContain('Hearthflame Mask')
+})
+
+// ▪️文字列型オブジェクトのパラメタライズドテストを作成
+const Ogerpon = ['Hearthflame Mask', 'Wellspring Mask', 'Cornerstone Mask']
+
+describe('test Ogerpon mask', () => {
+  test.each([
+    { mask: 'Hearthflame Mask' },
+    { mask: 'Wellspring Mask' },
+    { mask: 'Cornerstone Mask' },
+    ]) ('Ogerpon should have the correct mask', ({ mask }) => {
+    const result = Ogerpon.find(ogerponMask => ogerponMask.includes(mask));
+    // 期待されるマスクと実際のマスクを比較s
+    expect(result).toBe(mask);
+  });
+})
